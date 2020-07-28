daf1414fa0a7fb951d46d723c9bd60ebc0b336ab
Ian Gregg <iangregg188@gmail.com>
2020-07-28 10:41:05 -0700

    making chnages to the models and stepup.cs.
    co-authored-by: Cody Fritz <clanalia55@gmail.com>

--- ToDoList/Models/Item.cs
+++ ToDoList/Models/Item.cs
@@ -5,120 +5,20 @@ namespace ToDoList.Models
 {
   public class Item
   {
+    public int ItemId { get; set; }
     public string Description { get; set; }
-    public int Id { get; set; }
-
-    public Item(string description)
-    {
-      Description = description;
-    }
-
-    public Item(string description, int id)
-    {
-      Id = id;
-      Description = description;
-    }
-
-    public static List<Item> GetAll()
-    {
-      List<Item> allItems = new List<Item> { };
-      MySqlConnection conn = DB.Connection();
-      conn.Open();
-      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"SELECT * FROM items;";
-      MySqlDataReader rdr = cmd.ExecuteReader() as MySqlDataReader;
-      while (rdr.Read())
-      {
-          int itemId = rdr.GetInt32(0);
-          string itemDescription = rdr.GetString(1);
-          Item newItem = new Item(itemDescription, itemId);
-          allItems.Add(newItem);
-      }
-      conn.Close();
-      if (conn != null)
-      {
-          conn.Dispose();
-      }
-      return allItems;
-    }
-    
-    public override bool Equals(System.Object otherItem)
-    {
-      if (!(otherItem is Item))
-      {
-        return false;
-      }
-      else
-      {
-        Item newItem = (Item) otherItem;
-        bool idEquality = (this.Id == newItem.Id);
-        bool descriptionEquality = (this.Description == newItem.Description);
-        return (idEquality && descriptionEquality);
-      }
-    }
-
-    public static void ClearAll()
-    {
-      MySqlConnection conn = DB.Connection();
-      conn.Open();
-      var cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"DELETE FROM items;";
-      cmd.ExecuteNonQuery();
-      conn.Close();
-      if (conn != null)
-      {
-      conn.Dispose();
-      }
-    }
-    public static Item Find(int id)
-    {
-      MySqlConnection conn = DB.Connection();
-      conn.Open();
-
-      // We create MySqlCommand object and add a query to its CommandText property. We always need to do this to make a SQL query.
-      MySqlCommand cmd = conn.CreateCommand() as MySqlCommand;
-      cmd.CommandText = @"SELECT * FROM `items` WHERE id = @thisId;";
-
-      MySqlParameter thisId = new MySqlParameter();
-      thisId.ParameterName = "@thisId";
-      thisId.Value = id;
-      cmd.Parameters.Add(thisId);
-
-      var rdr = cmd.ExecuteReader() as MySqlDataReader;
-      int itemId = 0;
-      string itemDescription = ""; 
-      while(rdr.Read())
-      {
-        itemId =  rdr.GetInt32(0);
-        itemDescription = rdr.GetString(1);
-      }
-      Item foundItem = new Item(itemDescription, itemId);
-      conn.Close();
-      
-      if (conn != null)
-      {
-        conn.Dispose();
-      }
-      return foundItem;
-    }
-    public void Save()
-    {
-      MySqlConnection conn = DB.Connection();
-      conn.Open();
-      var cmd = conn.CreateCommand() as MySqlCommand;           // create a new command object
-      cmd.CommandText = @"INSERT INTO items (description) VALUES (@ItemDescription);";   //command object has a field of 'commandText'
-      MySqlParameter description = new MySqlParameter(); // instantiate new Parameter object
-      description.ParameterName = "@ItemDescription";   // defining the ParameterName of ^ object [parameter] 
-      description.Value = this.Description;    // definining the Value of this ^^ object [parameter]
-      cmd.Parameters.Add(description);      // adding the Parameter object to the Parameters field of the command object
-      cmd.ExecuteNonQuery();
-      Id = (int) cmd.LastInsertedId;
-      conn.Close();
-
-      if (conn != null)
-      {
-        conn.Dispose();
-      }
-    }
   }
+  public ActionResult Create()
+  {
+      return View();
+  }
+
+  [HttpPost]
+  public ActionResult Create(Item item)
+  {
+      _db.Items.Add(item);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+  }
+
 }
--- ToDoList/Startup.cs
+++ ToDoList/Startup.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList
 {
@@ -10,17 +11,20 @@ namespace ToDoList
   {
     public Startup(IHostingEnvironment env)
     {
-      var builder = new ConfigurationBuilder()
+        var builder = new ConfigurationBuilder()
           .SetBasePath(env.ContentRootPath)
-          .AddEnvironmentVariables();
-      Configuration = builder.Build();
+          .AddJsonFile("appsettings.json"); 
+        Configuration = builder.Build();
     }
 
-    public IConfigurationRoot Configuration { get; }
-
+    public IConfigurationRoot Configuration { get; set; }
     public void ConfigureServices(IServiceCollection services)
     {
       services.AddMvc();
+
+      services.AddEntityFrameworkMySql()
+        .AddDbContext<ToDoListContext>(options => options
+        .UseMySql(Configuration["ConnectionStrings:DefaultConnection"]));
     }
 
     public void Configure(IApplicationBuilder app)
@@ -41,10 +45,5 @@ namespace ToDoList
           await context.Response.WriteAsync("Something went wrong!");
         });
     }
-  }
-
-  public static class DBConfiguration
-  {
-    public static string ConnectionString = "server=localhost;user id=root;password=epicodus;port=3306;database=to_do_list;";
   }
 }
