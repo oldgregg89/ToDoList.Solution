7e51153e1ca807e7ca28ae1cf457f44a2f7effb6
Ian Gregg <iangregg188@gmail.com>
2020-07-29 09:54:27 -0700

    working on the controllers.
    co-authored-by: Cody Fritz <clanalia55@gmail.com>

--- ToDoList/Controllers/ItemsController.cs
+++ ToDoList/Controllers/ItemsController.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Mvc;
 using ToDoList.Models;
 using System.Collections.Generic;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 
 namespace ToDoList.Controllers
 {
@@ -10,7 +11,7 @@ namespace ToDoList.Controllers
     private readonly ToDoListContext _db;
     public ItemsController(ToDoListContext db)
     {
-_db = db;
+      _db = db;
     }
     public ActionResult Index()
     {
@@ -21,13 +22,39 @@ _db = db;
     public ActionResult Details( int id )
     {
       Item thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
-return View(thisItem);
+      return View(thisItem);
     }
     public ActionResult Create()
     {
         return View();
     }
+    public ActionResult Edit (int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
+    
+    public ActionResult Delete(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      return View(thisItem);
+    }
 
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisItem = _db.Items.FirstOrDefault(items => items.ItemId == id);
+      _db.Items.Remove(thisItem);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+    [HttpPost]
+    public ActionResult Edit(Item item)
+    {
+      _db.Entry(item).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
     [HttpPost]
     public ActionResult Create(Item item)
     {
